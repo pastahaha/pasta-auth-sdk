df50e235756270dec6adf729bda51a9eb19de3bd
AdityaPrashantPashte <prashant@authnull.com>
2025-07-30 17:34:18 +0530

    flow

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="mcp-oauth-sdk",
     version="0.1.0",
-    description="OAuth SDK for MCP Server (Google OIDC)",
+    description="Google OAuth SDK for MCP",
     packages=find_packages(),
     install_requires=[
         "requests",
